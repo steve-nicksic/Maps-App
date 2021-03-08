08d90b3d83d080881d672279031e6dbb8b6811bb
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-03-08 14:24:47 -0600

    Updated javascript with coordinates variable, truned off scrollwheel, added map marker image, bounce feature, and info window.

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,7 +1,29 @@
 var map;
+var coords = {lat: 32.5252, lng: -93.7502};
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 32.5252, lng: -93.7502},
+        center: coords, // this can be specific coordinates or setup with variable as we have it here
         zoom: 10
+        scrollwheel: false
     });
+
+    var marker = new google.maps.Marker({
+    position: coords,
+    map: map,
+    icon: image,
+    animation: google.maps.Animation.Bounce
+    });
+
+    var image = {url: '/enterprise1.png',
+        scaledSize: new google.maps.Size(50, 50)};
+
+    var contentString = '<h2>Shreveport, LA</h2> <p>Where your dreams come true.</p>';
+
+     var infowindow = new google.maps.InfoWindow({
+       content: contentString
+     });
+
+     google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+     });
 }
